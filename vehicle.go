2ae3237b093178d917784afd63d8cdd06ab8e285
Eran Amarante <eran.amarante@yahoo.com.ph>
2021-07-29 02:09:33 +0800

    fixed err

--- vehicle.go
+++ vehicle.go
@@ -1,22 +1,23 @@
 package main
 
+type vehicle interface{}
+
 // Values array for the feedback.json file
 type Values struct {
 	Models []Model `json:"values"`
-	Name string `json:"model"`
-	Feedback []string `json:"feedback"`
 }
 
 type feedbackResult struct {
-	feedbackTotal int
+	feedbackTotal    int
 	feedbackPositive int
 	feedbackNegative int
-	feedbackNeutral int
+	feedbackNeutral  int
 }
 
 // Model array for the feedback.json file
 type Model struct {
-	
+	Name     string   `json:"model"`
+	Feedback []string `json:"feedback"`
 }
 
 type rating float32
@@ -30,24 +31,23 @@ const (
 )
 
 type car struct {
-	model string
-	make string
+	model       string
+	make        string
 	typeVehicle string
 }
 
 type truck struct {
-	model string
-	make string
+	model       string
+	make        string
 	typeVehicle string
 }
 
 type bike struct {
 	model string
-	make string
+	make  string
 }
 
 func init() {
-
 
 	inventory = []vehicle{
 		bike{"FTR 1200", "Indian"},
@@ -69,7 +69,7 @@ func init() {
 func main() {
 
 	// Generate ratings for the different vehicles
-	
+
 	// Print ratings for the different vehicles
 }
 
