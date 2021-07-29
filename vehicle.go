ea7ed42b3bc449855c9ec4666763ab9e0c050dca
Eran Amarante <eran.amarante@yahoo.com.ph>
2021-07-29 20:14:12 +0800

    fixed switch statement

--- vehicle.go
+++ vehicle.go
@@ -99,42 +99,39 @@ func readJSONFile() Values {
 	return content
 }
 
-func generateRating()  {
-	f := readJSONfile()
-	for _, v := range f.Models {
+func generateRating() {
+
+	f := readJSONFile()
+
+	for _,v:= range f.Models {
 		var vehResult feedbackResult
 		var vehRating rating
-
 		for _, msg := range v.Feedback {
-			
-			if text := strings.Split(msg, " "); len(text) >= 5 {
+			if text := strings.Split(msg," "); len(text) >= 5 {
 				vehRating = 5.0
 				vehResult.feedbackTotal++
-
-				for _, word := range text {
-					switch s:=strings.Trim(strings.ToLower(word), " ,.,!,?,\t,\n,\r") {
+				for _,word := range text {
+					switch s:=strings.Trim(strings.ToLower(word), " ,.,!,?,\t,\n,\r"); s {
 					case "pleasure", "impressed", "wonderful", "fantastic", "splendid":
 						vehRating += extraPositive
 					case "help", "helpful", "thanks", "thank you", "happy":
-									vehRating += positive
+						vehRating += positive
 					case "not helpful", "sad", "angry", "improve", "annoy":
-									vehRating += negative
+						vehRating += negative
 					case "pathetic", "bad", "worse", "unfortunately", "agitated", "frustrated":
-									vehRating += extraNegative
+						vehRating += extraNegative	
 					}
 				}
-
 				switch {
 				case vehRating > 8.0:
-						vehResult.feedbackPositive++
+					vehResult.feedbackPositive++
 				case vehRating >= 4.0 && vehRating <= 8.0:
-						vehResult.feedbackNeutral++
+					vehResult.feedbackNeutral++
 				case vehRating < 4.0:
-						vehResult.feedbackNegative++
+					vehResult.feedbackNegative++
 				}
 			}
 		}
-
-		vehResult =vehicleResult[v.Name]
+		vehicleResult[v.Name] = vehResult
 	}
 }
